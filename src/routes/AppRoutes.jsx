03fe916fcf6a505a2adb3379d29ed068ca8bca9b
Hananta <ontapluges122@gmail,com>
2025-07-03 02:35:07 +0800

    menambah fitur CartPage

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import DashboardPage from '../pages/Dashboard/DashboardPage';
 import StoreListPage from '../pages/Store/StoreListPage';
 import UserManagement from '../pages/UserManagement';
-
+import CartPage from '../pages/CartPage/CartPage';
+import PaymentMethodPage from '../pages/Payments/PaymentMethodPage';
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<DashboardPage />} />
       <Route path="/store" element={<StoreListPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/payment-method" element={<PaymentMethodPage />} />
       <Route path="/user-management" element={<UserManagement />} />
     </Routes>
   );
