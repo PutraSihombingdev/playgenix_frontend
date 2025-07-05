1f638c1640dc391dcdcaa851d01c09d03c2df3a1
PutraSihombingdev <putrahombing17@gmail.com>
2025-07-05 12:29:32 +0800

    Perubahan design dasboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import DashboardPage from '../pages/Dashboard/DashboardPage';
 import StoreListPage from '../pages/Store/StoreListPage';
-import UserManagement from '../pages/UserManagement';
+
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<DashboardPage />} />
       <Route path="/store" element={<StoreListPage />} />
-      <Route path="/user-management" element={<UserManagement />} />
+
     </Routes>
   );
 }
